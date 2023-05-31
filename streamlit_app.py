cdb7a8960cb6ade5c2d5603fb5eabf5fa0070c3d
chrismirvana27 <58166566+chrismirvana27@users.noreply.github.com>
2023-05-31 14:41:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Sets rows and columns
@@ -47,3 +46,4 @@ streamlit.dataframe(my_data_rows)
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
 my_data_rows = my_data_rows.append(fruit_choice)
+streamlit.dataframe(my_data_rows)
